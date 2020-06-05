fff2fe81559ee2fe820a95a7d86c889dbece0b82
Andrew Thomas <andrew.bower.thomas@gmail.com>
2020-06-05 12:52:55 -0500

    Implement tabs.

---
+++ DEVGROUPWEB/script.js
@@ -0,0 +1,17 @@
+﻿function openPage(pageName, elmnt, color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(pageName).style.display = "block";
+    elmnt.style.backgroundColor = "transparent";
+    elmnt.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
